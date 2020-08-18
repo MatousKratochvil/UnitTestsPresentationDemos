eea64feefc559bbdaa56f2a1f74cfd9ca08853f2
Matouš Kratochvíl <matous.B89@live.com>
2020-08-18 22:59:47 +0200

    Add example of intergration test

---
+++ UnitTestsPresentationDemos/Implementation/IntegrationService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+using UnitTestsPresentationDemos.Abstraction;
+using UnitTestsPresentationDemos.Requests;
+using UnitTestsPresentationDemos.Responses;
+
+namespace UnitTestsPresentationDemos.Implementation
+{
+	public class IntegrationService : IService
+	{
+		readonly IRepository repository;
+
+		public IntegrationService(IRepository repository)
+		{
+			this.repository = repository ?? throw new System.ArgumentNullException(nameof(repository));
+		}
+
+		public IEnumerable<Response> GetAll(Request request) => repository.GetAll(request);
+		public Response Save(Request request) => repository.Save(request);
+	}
+}
---
+++ UnitTestsPresentationDemosTests/IntegrationTest.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Linq;
+
+using UnitTestsPresentationDemos.Implementation;
+using UnitTestsPresentationDemos.Requests;
+
+using Xunit;
+
+namespace UnitTestsPresentationDemosTests
+{
+	public class IntegrationTest
+	{
+		[Fact]
+		public void ServiceRepositoryIntegration()
+		{
+			// Arrange
+			var repository = new IntegrationRepository();
+			var service = new IntegrationService(repository);
+
+			// Act
+			var response = service.GetAll(new Request());
+
+			// Assert
+			Assert.Equal(100, response.Count());
+		}
+	}
+}
